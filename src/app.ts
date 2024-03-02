163d3af9a6c6744be258f726999991fc67b03ee2
Dynylson Júnior <dynylsonjr@gmail.com>
2024-03-02 17:06:10 -0300

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Junior',
+    email: 'teste@teste.com',
+  },
+})
